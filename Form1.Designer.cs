2ceb678947d0799acf20acf1c4c523daee617789
Drenalol <acidlol@live.ru>
2016-05-13 00:32:24 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tochka = new System.Windows.Forms.Button();
+            this.fails = new System.Windows.Forms.TextBox();
+            this.hist = new System.Windows.Forms.RichTextBox();
+            this.kzarka = new System.Windows.Forms.TextBox();
+            this.primerno = new System.Windows.Forms.Label();
+            this.enchant1000 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cnt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tochka
+            // 
+            this.tochka.Location = new System.Drawing.Point(17, 90);
+            this.tochka.Name = "tochka";
+            this.tochka.Size = new System.Drawing.Size(95, 23);
+            this.tochka.TabIndex = 0;
+            this.tochka.Text = "Enchant once";
+            this.tochka.UseVisualStyleBackColor = true;
+            this.tochka.Click += new System.EventHandler(this.tochka_Click);
+            // 
+            // fails
+            // 
+            this.fails.Location = new System.Drawing.Point(98, 64);
+            this.fails.Name = "fails";
+            this.fails.Size = new System.Drawing.Size(75, 20);
+            this.fails.TabIndex = 2;
+            this.fails.Text = "0";
+            this.fails.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // hist
+            // 
+            this.hist.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.hist.Location = new System.Drawing.Point(0, 119);
+            this.hist.Name = "hist";
+            this.hist.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.hist.Size = new System.Drawing.Size(510, 487);
+            this.hist.TabIndex = 5;
+            this.hist.Text = "";
+            // 
+            // kzarka
+            // 
+            this.kzarka.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.kzarka.Location = new System.Drawing.Point(98, 5);
+            this.kzarka.Name = "kzarka";
+            this.kzarka.Size = new System.Drawing.Size(75, 53);
+            this.kzarka.TabIndex = 7;
+            this.kzarka.Text = "0";
+            this.kzarka.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // primerno
+            // 
+            this.primerno.AutoSize = true;
+            this.primerno.Location = new System.Drawing.Point(179, 67);
+            this.primerno.Name = "primerno";
+            this.primerno.Size = new System.Drawing.Size(14, 13);
+            this.primerno.TabIndex = 8;
+            this.primerno.Text = "~";
+            // 
+            // enchant1000
+            // 
+            this.enchant1000.Location = new System.Drawing.Point(118, 90);
+            this.enchant1000.Name = "enchant1000";
+            this.enchant1000.Size = new System.Drawing.Size(75, 23);
+            this.enchant1000.TabIndex = 9;
+            this.enchant1000.Text = "Enchant";
+            this.enchant1000.UseVisualStyleBackColor = true;
+            this.enchant1000.Click += new System.EventHandler(this.enchant1000_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(423, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "CLS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(315, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(183, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Source data (Korea, OLD maybe)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Lvl. enchant";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Fails";
+            // 
+            // cnt
+            // 
+            this.cnt.Location = new System.Drawing.Point(199, 92);
+            this.cnt.Name = "cnt";
+            this.cnt.Size = new System.Drawing.Size(58, 20);
+            this.cnt.TabIndex = 15;
+            this.cnt.Text = "1000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(263, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "times.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 606);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cnt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.enchant1000);
+            this.Controls.Add(this.primerno);
+            this.Controls.Add(this.kzarka);
+            this.Controls.Add(this.hist);
+            this.Controls.Add(this.fails);
+            this.Controls.Add(this.tochka);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(526, 645);
+            this.MinimumSize = new System.Drawing.Size(526, 645);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BDO Enchant Simulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button tochka;
+        private System.Windows.Forms.TextBox fails;
+        private System.Windows.Forms.RichTextBox hist;
+        private System.Windows.Forms.TextBox kzarka;
+        private System.Windows.Forms.Label primerno;
+        private System.Windows.Forms.Button enchant1000;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox cnt;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Enchant(int n, bool ths)
+        {
+            var baseChance = new List<double>
+            {
+                100, //1
+                100, //2
+                100, //3
+                100, //4
+                100, //5
+                100, //6
+                100, //7
+                20, //8
+                17.5, //9
+                15, //10
+                12.5, //11
+                10, //12
+                7.5, //13
+                5, //14
+                2.5, //15
+                15, //16
+                7.5, //17
+                5, //18
+                2, //19
+                1.5 //20
+            };
+            var failUpChance = new List<double>
+            {
+                0, //1
+                0, //2
+                0, //3
+                0, //4
+                0, //5
+                0, //6
+                0, //7
+                2.5, //8
+                2, //9
+                1.5, //10
+                1.25, //11
+                0.75, //12
+                0.63, //13
+                0.5, //14
+                0.5, //15
+                1.5, //16
+                0.75, //17
+                0.5, //18
+                0.25, //19
+                0.15 //20
+            };
+            var maxFails = new List<int>
+            {
+                0, //1
+                0, //2
+                0, //3
+                0, //4
+                0, //5
+                0, //6
+                0, //7
+                13, //8
+                14, //9
+                15, //10
+                16, //11
+                18, //12
+                20, //13
+                25, //14
+                25, //15
+                25, //16
+                35, //17
+                44, //18
+                90, //19
+                124 //20
+            };
+            var failatfail = new List<int>
+            {
+                1, //1
+                1, //2
+                1, //3
+                1, //4
+                1, //5
+                1, //6
+                1, //7
+                1, //8
+                1, //9
+                1, //10
+                1, //11
+                1, //12
+                1, //13
+                1, //14
+                1, //15
+                2, //16
+                3, //17
+                4, //18
+                5, //19
+                6 //20
+            };
+            if (ths)
+            {
+                fails.Tag = fails.Text;
+                var skolko = 0;
+                var tochkana = Convert.ToInt32(kzarka.Text);
+                var ebanniyRandom = new Random();
+                var list = new List<int>();
+                for (var i = 0; i < Convert.ToInt32(cnt.Text); i++)
+                {
+                    var end = 0;
+                    fails.Text = (string) fails.Tag;
+                    while (end == 0)
+                    {
+                        var zatochil = ebanniyRandom.Next(0, 100);
+                        double chance;
+                        if (Convert.ToInt32(fails.Text) >= maxFails[n])
+                        {
+                            chance = baseChance[n] + (maxFails[n] * failUpChance[n]);
+                        }
+                        else
+                        {
+                            chance = baseChance[n] + (Convert.ToDouble(fails.Text) * failUpChance[n]);
+                        }
+                        if (zatochil <= chance)
+                        {
+                            list.Add(Convert.ToInt32(fails.Text));
+                            skolko++;
+                            end = 1;
+                        }
+                        else
+                        {
+                            fails.Text = Convert.ToString(Convert.ToInt32(fails.Text) + failatfail[n]);
+                        }
+                    }
+                    Thread.Sleep(10);
+                }
+                fails.Text = (string)fails.Tag;
+                hist.AppendText($"Enchant {tochkana + 1} done. Starts from {fails.Tag}, AVG: {Math.Round((double)list.Sum() / list.Count)}, MIN: {list.Min()}, MAX: {list.Max()}.\r\n");
+                enchant1000.Enabled = true;                
+            }
+            else
+            {
+                var ebanniyRandom = new Random();
+                double chance;
+                var tochkana = Convert.ToInt32(kzarka.Text);
+                if (Convert.ToInt32(fails.Text) >= maxFails[n])
+                {
+                    chance = baseChance[n] + (maxFails[n]*failUpChance[n]);
+                }
+                else
+                {
+                    chance = baseChance[n] + (Convert.ToDouble(fails.Text)*failUpChance[n]);
+                }
+                var zatochil = ebanniyRandom.Next(0, 100);
+                primerno.Text = $"~{chance.ToString(CultureInfo.InvariantCulture)}%";
+                if (zatochil <= chance)
+                {
+                    kzarka.Text = Convert.ToString(tochkana + 1);
+                    hist.AppendText($"Enchant {kzarka.Text} done at {fails.Text} fails.\r\n");
+                    fails.Text = @"0";
+                    primerno.Text = @"~";
+                }
+                else
+                {
+                    fails.Text = Convert.ToString(Convert.ToInt32(fails.Text) + failatfail[n]);
+                }
+            }
+        }
+
+        private void tochka_Click(object sender, EventArgs e)
+        {
+            var tochkana = Convert.ToInt32(kzarka.Text);
+            if (tochkana < 20) Enchant(tochkana,false);
+        }
+
+        private void enchant1000_Click(object sender, EventArgs e)
+        {
+            ((Button)sender).Enabled = false;            
+            var tochkana = Convert.ToInt32(kzarka.Text);
+            if (tochkana < 20) Enchant(tochkana, true);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hist.Clear();
+            kzarka.Text = @"0";
+            fails.Text = @"0";
+            primerno.Text = @"~";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start(
+                "http://www.inven.co.kr/board/powerbbs.php?come_idx=3584&name=subject&keyword=%B0%AD%C8%AD&l=19419");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
